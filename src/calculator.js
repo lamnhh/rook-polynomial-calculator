2a890b982d3fde8fbfe7298627828f243e07a093
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-11-30 00:56:52 +0700

    Updated new dp formula

--- src/calculator.js
+++ src/calculator.js
@@ -1,57 +1,47 @@
-const compute = (mask) => {
-  const n = mask.length;
-  const m = mask[0].length;
+const compute = (a) => {
+  const n = a.length;
+  const m = a[0].length;
   const k = Math.min(n, m);
-  let firstOneX = -1;
-  let firstOneY = -1;
-  for (let i = 0; i < n && firstOneX === -1; ++i) {
-    for (let j = 0; j < m; ++j) {
-      if (mask[i][j] === 1) {
-        firstOneX = i;
-        firstOneY = j;
-        break;
+
+  // Create dp array of size (n x 2^m)
+  const dp = Array.from(Array(n), () => Array.from(Array(1 << m), () => 0));
+
+  // Initialize dp array
+  // Attempt to put one rook at every 1 position in the first row.
+  dp[0][0] = 1;
+  for (let j = 0; j < m; ++j) {
+    if (a[0][j] == 1) {
+      dp[0][1 << j] = 1;
+    }
+  }
+
+  for (let i = 1; i < n; ++i) {
+    for (let mask = 0; mask < 1 << m; ++mask) {
+      // Case 1: ignore the entire i-th row
+      dp[i][mask] = dp[i - 1][mask];
+
+      // Case 2: choose exactly one element in the i-th row
+      for (let j = 0; j < m; ++j) {
+        if (((mask >> j) & 1) == 0 || a[i][j] == 0) {
+          continue;
+        }
+        const prev_mask = mask - (1 << j);
+        dp[i][mask] = dp[i][mask] + dp[i - 1][prev_mask];
       }
     }
   }
 
-  if (firstOneX === -1) {
-    const ans = Array.from(Array(k + 1), () => 0);
-    ans[0] = 1;
-    return ans;
-  }
-
-  const copyMask = [];
-  for (let i = 0; i < n; ++i) {
-    const arr = [];
-    for (let j = 0; j < m; ++j) {
-      arr.push(mask[i][j]);
-    }
-    copyMask.push(arr);
-  }
-
-  copyMask[firstOneX][firstOneY] = 0;
-  const dp1 = compute(copyMask);
-
-  for (let i = 0; i < n; ++i) {
-    for (let j = 0; j < m; ++j) {
-      copyMask[i][j] = mask[i][j];
-    }
-  }
-  for (let i = 0; i < n; ++i) {
-    copyMask[i][firstOneY] = 0;
-  }
-  for (let j = 0; j < m; ++j) {
-    copyMask[firstOneX][j] = 0;
-  }
-  const dp2 = compute(copyMask);
-
   const ans = Array.from(Array(k + 1), () => 0);
-  for (let i = 0; i <= k; ++i) {
-    ans[i] = dp1[i];
-    if (i > 0) {
-      ans[i] += dp2[i - 1];
+  for (let mask = 0; mask < 1 << m; ++mask) {
+    let cnt = 0;
+    for (let i = 0; i < m; ++i) {
+      cnt += (mask >> i) & 1;
+    }
+    if (cnt <= k) {
+      ans[cnt] += dp[n - 1][mask];
     }
   }
+
   return ans;
 };
 
